5ea5dfc23264d7024612a41e48521775f007106f
ArthurLS <arthur.lesaint@hotmail.fr>
2019-01-10 22:32:49 +0100

    camera monitoring

---
+++ Assets/Scripts/SteadyCamTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteadyCamTarget : MonoBehaviour {
+
+    public GameObject TheVehicule;
+    private float VehiculeX;
+    private float VehiculeY;
+    private float VehiculeZ;
+
+	// Update is called once per frame
+	void Update () {
+        VehiculeX = TheVehicule.transform.eulerAngles.x;
+        VehiculeY = TheVehicule.transform.eulerAngles.y;
+        VehiculeZ = TheVehicule.transform.eulerAngles.z;
+
+        transform.eulerAngles = new Vector3(VehiculeX - VehiculeX, VehiculeY, VehiculeZ - VehiculeZ);
+    }
+}
---
+++ Assets/Scripts/shipBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipBehavior : MonoBehaviour {
+
+    Rigidbody rigidbody = null;
+    Vector3 euler_y;
+    Vector3 euler_y2;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+
+        //Set the axis the Rigidbody rotates in (100 in the y axis)
+        euler_y = new Vector3(0, 0, 123);
+        euler_y2 = new Vector3(0, 0, -123);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        { 
+            rigidbody.AddForce(transform.up * 9, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody.AddForce(transform.up * -9, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Quaternion deltaRotation = Quaternion.Euler(euler_y2 * Time.deltaTime);
+            rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Quaternion deltaRotation = Quaternion.Euler(euler_y * Time.deltaTime);
+            rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
+        }
+
+    }
+}
